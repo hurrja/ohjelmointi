a76ada1617d1f832338cb3a124573a7a0443ebd1
Jarmo Hurri <jarmo.hurri@iki.fi>
2016-06-10 16:29:07 +0300

    * lataa tiedot kappaleista ja tekee (rumat) linkit (rumalle) taustalle

--- etusivu.js
+++ etusivu.js
@@ -1,18 +1,43 @@
 var canvas;
+var kappaletiedostot;
+var kappaleidenNimet;
+var kappaleita;
+var kappaleElementit;
 
-function setup()
+function preload ()
 {
+    kappaletiedostot = loadStrings ("kappaletiedostot.txt");
+    kappaleidenNimet = loadStrings ("kappaleiden-nimet.txt");
+}
+
+function setup ()
+{
+    kappaleita = kappaletiedostot.length;
+    kappaleElementit = new Array ();
     canvas = createCanvas (windowWidth, windowHeight);
     colorMode (HSB, 100);
-    
+    noLoop ();
 }
 
 function draw ()
 {
-    background (17, 100, 100);
+    removeElements ();
+    background (17, 50, 100);
+    var x = windowWidth / 5;
+    var korkeus = windowHeight / (2 * kappaleita + 1);
+    
+    for (i = 0; i < kappaleita; i++)
+    {
+        var elementti = createA (kappaletiedostot [i] + ".html",
+                                 kappaleidenNimet [i]);
+        elementti.position (x, (2 * i + 1) * korkeus);
+        elementti.size (AUTO, korkeus);
+        kappaleElementit [i] = elementti;
+    }
 }
 
-window.onresize = function ()
+function windowResized ()
 {
-    canvas.size (windowWidth, windowHeight);
-};
+    resizeCanvas (windowWidth, windowHeight);
+}
+
